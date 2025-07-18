1879f305f371f61a0bad6b7e683c72e97b0a54ed
Satish Srinivasan <vasan.s@me.com>
2025-07-18 22:08:37 +0530

    WIP

--- src/test/java/me/vasan/jimple/EnvironmentTest.java
+++ src/test/java/me/vasan/jimple/EnvironmentTest.java
@@ -19,4 +19,19 @@ public class EnvironmentTest {
         Assert.assertEquals(childEnv.get("world"), 32);
         Assert.assertEquals(childEnv.get("hello"), 32);
     }
+
+    @Test
+    public void testEnv2() {
+        Environment parentEnv = new Environment();
+        Environment childEnv = new Environment(parentEnv);
+        parentEnv.put("hello", 23);
+        parentEnv.put("world", 32);
+        childEnv.put("hello", 32);
+        Assert.assertNotNull(parentEnv);
+        Assert.assertNotNull(childEnv);
+        Assert.assertTrue(parentEnv.exists("hello"));
+        Assert.assertTrue(childEnv.exists("hello"));
+        Assert.assertEquals(childEnv.get("world"), 32);
+        Assert.assertEquals(childEnv.get("hello"), 32);
+    }
 }
